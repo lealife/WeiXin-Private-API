5b9daa23e44faff701f383ae91cbea9d176cba5d
life <lifephp@gmail.com>
2013-08-25 14:19:56 +0800

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 $G_ROOT = dirname(__FILE__);
 
 $G_CONFIG["weiXin"] = array(
-	'account' => '公共平台账号',
+	'account' => '公众平台账号',
 	'password' => '密码',
 	'cookiePath' => $G_ROOT. '/cache/cookie', // cookie缓存文件路径
 	'webTokenPath' => $G_ROOT. '/cache/webToken', // webToken缓存文件路径
