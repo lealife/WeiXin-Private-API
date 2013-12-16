2ef16949aa163bed69329f1e3e92008e645131af
lifephp@gmail.com <lifephp@gmail.com>
2013-12-16 15:31:56 +0800

    just for test2

--- config.php
+++ config.php
@@ -5,8 +5,8 @@
 $G_ROOT = dirname(__FILE__);
 
 $G_CONFIG["weiXin"] = array(
-	'account' => '公众平台账号',
-	'password' => '密码',
+	'account' => ' 公众平台账号 ',
+	'password' => ' 密码 ',
 	'cookiePath' => $G_ROOT. '/cache/cookie', // cookie缓存文件路径
 	'webTokenPath' => $G_ROOT. '/cache/webToken', // webToken缓存文件路径
 );
